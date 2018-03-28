5fc974a77eec62436ae8c818855192aa5892de08
Denniston Sutherland <bigjoe766@gmail.com>
2018-03-28 01:42:20 +0100

    Convert to MVC design pattern

---
+++ Todooey/Model/Item.swift
@@ -0,0 +1,33 @@
+//
+//  Item.swift
+//  Todooey
+//
+//  Created by Denniston Sutherland on 28/03/2018.
+//  Copyright © 2018 DennistonSutherland. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
